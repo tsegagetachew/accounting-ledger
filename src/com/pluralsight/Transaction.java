9c0f2de0abc7718c1ff2e747619a0e8f72e157d1
Tsega <tsegagetachew.tg@gmail.com>
2026-04-29 09:55:18 -0400

    Initial commit

---
+++ src/com/pluralsight/Transaction.java
@@ -0,0 +1,39 @@
+package com.pluralsight;
+
+public class Transaction {
+    private String date, time, description, vendor; //constructors
+    private double amount;
+
+    public Transaction(String date, String time, String description, String vendor, double amount) {  //getters
+        this.date = date;
+        this.time = time;
+        this.description = description;
+        this.vendor = vendor;
+        this.amount = amount;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getVendor() {
+        return vendor;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+
+
+
+
+}
